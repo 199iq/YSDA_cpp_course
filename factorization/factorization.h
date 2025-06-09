4538411a785dc61bf266b48c50b7245cd1476131
Said Abdulaev <sabdulaev15@gmail.com>
2025-06-09 10:21:44 +0300

    Add solutions for factorization.h & rotate.h

--- factorization/factorization.h
+++ factorization/factorization.h
@@ -6,5 +6,22 @@
 #include <stdexcept>
 
 std::vector<std::pair<int64_t, int>> Factorize(int64_t x) {
-    throw std::runtime_error{"Not implemented"};
+    std::vector<std::pair<int64_t, int>> ans;
+    int64_t tmp = x;
+
+    for (int64_t d = 2; d * d <= x; d++) {
+        int cnt = 0;
+        while (tmp % d == 0) {
+            tmp /= d;
+            cnt++;
+        }
+        if (cnt > 0) {
+            ans.emplace_back(d, cnt);
+        }
+    }
+    if (tmp > 1) {
+        ans.emplace_back(tmp, 1);
+    }
+
+    return ans;
 }
--- rotate/rotate.h
+++ rotate/rotate.h
@@ -4,6 +4,26 @@
 #include <vector>
 #include <cstddef>
 
+void Reverse(std::vector<int>* data, size_t left, size_t right) {
+    std::vector<int>& vec = *data;
+
+    size_t mid = (left + right + 1) / 2;
+    for (size_t i = left; i < mid; i++) {
+        size_t next = right - (i - left);
+
+        std::swap(vec[i], vec[next]);
+    }
+}
+
 void Rotate(std::vector<int>* data, size_t shift) {
-    throw std::runtime_error{"Not implemented"};
+    std::vector<int>& vec = *data;
+
+    size_t n = vec.size();
+    if (!n || !shift) {
+        return;
+    }
+
+    Reverse(data, 0, shift - 1);
+    Reverse(data, shift, n - 1);
+    Reverse(data, 0, n - 1);
 }
